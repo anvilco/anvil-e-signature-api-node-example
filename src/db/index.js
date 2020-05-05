5fe849244350f4d9e77c8ba73f24f845af7d59ec
Evan Dweck <evanjordandweck@gmail.com>
2020-05-05 12:57:28 -0700

    add a fake db w/ teamMembers for now

---
+++ src/db/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  teamMembers: [
+    {
+      id: '1',
+      firstName: 'Mang-Git',
+      lastName: 'Ng',
+      favoriteEmoji: '🦕',
+    },
+    {
+      id: '2',
+      firstName: 'Ben',
+      lastName: 'Ogle',
+      favoriteEmoji: '🌵',
+    },
+    {
+      id: '3',
+      firstName: 'Evan',
+      lastName: 'Dweck',
+      favoriteEmoji: '🐉',
+    },
+    {
+      id: '3',
+      firstName: 'Richard',
+      lastName: 'Albayaty',
+      favoriteEmoji: '👾',
+    },
+  ],
+}
