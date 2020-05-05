ff29eebf36861c7df1cb4b87c9bebea04f90f17c
Evan Dweck <evanjordandweck@gmail.com>
2020-05-05 13:02:15 -0700

    :art:

--- src/test/server/index.test.js
+++ src/test/server/index.test.js
@@ -1,9 +1,11 @@
 const { expect } = require('chai')
 
-describe('Dummy test', function () {
-  it('expect mocha & sinon to work', function () {
-    const testValue = 10
-    expect(testValue).to.not.be.null
-    expect(testValue).to.equal(10)
+describe('Server Tests', function () {
+  describe('Dummy test', function () {
+    it('expect mocha & sinon to work', function () {
+      const testValue = 10
+      expect(testValue).to.not.be.null
+      expect(testValue).to.equal(10)
+    })
   })
 })
