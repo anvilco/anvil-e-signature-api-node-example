c11939e2866fce5e3ee9f35695acd14ad7131c85
Ben Ogle <ogle.ben@gmail.com>
2020-05-06 10:34:08 -0700

    fix ids

--- src/db/index.js
+++ src/db/index.js
@@ -1,28 +1,28 @@
 module.exports = {
   teamMembers: [
     {
-      id: '1',
+      id: 1,
       firstName: 'Mang-Git',
       lastName: 'Ng',
       favoriteEmoji: '🦕',
       imgPath: '/src/client/static/img/mg.jpg',
     },
     {
-      id: '2',
+      id: 2,
       firstName: 'Ben',
       lastName: 'Ogle',
       favoriteEmoji: '🌵',
       imgPath: '/src/client/static/img/bo.jpg',
     },
     {
-      id: '3',
+      id: 3,
       firstName: 'Evan',
       lastName: 'Dweck',
       favoriteEmoji: '🐉',
       imgPath: '/src/client/static/img/ed.jpg',
     },
     {
-      id: '3',
+      id: 4,
       firstName: 'Richard',
       lastName: 'Albayaty',
       favoriteEmoji: '👾',
