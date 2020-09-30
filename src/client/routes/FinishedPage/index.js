9ce07be2bf47a35ddb3f56f759a00773d6b335c3
Ben Ogle <ogle.ben@gmail.com>
2020-09-30 14:43:29 -0700

    Use the apiBaseURL in the UI

--- src/client/routes/FinishedPage/index.js
+++ src/client/routes/FinishedPage/index.js
@@ -53,7 +53,7 @@ const FinishedPage = () => {
             type="orange"
           >
             <StyledAnchor
-              href={`http://localhost:3000/org/${results?.organizationSlug}/etch/${results?.etchPacketEid}`}
+              href={buildAnvilURL(`/org/${results?.organizationSlug}/etch/${results?.etchPacketEid}`)}
               target="_blank"
               rel="noreferrer"
             >
@@ -84,7 +84,7 @@ const FinishedPage = () => {
             type="cta"
           >
             <StyledAnchor
-              href={`http://localhost:3000/api/etch/download/${results?.documentGroupEid}/${results?.etchTemplateEid}.zip`}
+              href={buildAnvilURL(`/api/etch/download/${results?.documentGroupEid}/${results?.etchTemplateEid}.zip`)}
             >
               Download Documents as Zip File
             </StyledAnchor>
@@ -128,6 +128,10 @@ const FinishedPage = () => {
       <StyledLink to="/">Return</StyledLink>
     </>
   )
+}
+
+function buildAnvilURL (url) {
+  return window.apiBaseURL + url
 }
 
 export default FinishedPage
