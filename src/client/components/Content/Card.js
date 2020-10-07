1ed5d3f4dc85ef22a806aed8cd9df3a8f1a227bc
Ben Ogle <ogle.ben@gmail.com>
2020-10-07 15:54:09 -0700

    Add layout component

--- src/client/components/Content/Card.js
+++ src/client/components/Content/Card.js
@@ -4,11 +4,8 @@ import theme from 'theme'
 import { ListContainer } from './List'
 
 const Card = styled.div`
-  background: ${({ layout }) => layout ? '#fdfdfd' : 'white'};
-  max-width: ${({ layout }) => layout ? '1000px' : ''};
-  min-height: ${({ layout }) => layout ? '1000px' : ''};
-  margin: ${({ layout }) => layout ? '0 auto' : ''};
-  padding: ${({ layout }) => layout ? '130px' : `${theme.paddings[20]}px`};
+  background: white;
+  padding: ${theme.paddings[20]}px;
   box-shadow: ${theme.shadows[50]};
 
   margin-bottom: ${theme.paddings[20]}px;
