dc2f2e04779912605adad7a3c09576cc4e3f1469
Winggo Tse <winggo.tse@gmail.com>
2020-12-04 10:31:02 -0800

    remove cors

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ const path = require('path')
 const express = require('express')
 const fallback = require('express-history-api-fallback')
 const appModulePath = require('app-module-path')
-const cors = require('cors')
 const { logError } = require('./helpers')
 const { apiKey, port: PORT, environment } = require('../config')
 
@@ -12,7 +11,6 @@ appModulePath.addPath(path.join(__dirname, '..', '..', 'src'))
 const routes = require('./routes')
 const app = express()
 
-app.use(cors())
 app.use(express.json({
   inflate: true,
   limit: '20mb',
